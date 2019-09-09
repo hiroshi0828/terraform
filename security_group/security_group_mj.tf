f642196ca14d645e8c294d267eb2a781b7c92d40
hiroshi0828 <abudieru@gmail.com>
2019-09-09 14:48:18 +0900

    alb作成

--- security_group/security_group_mj.tf
+++ security_group/security_group_mj.tf
@@ -10,6 +10,7 @@ resource "aws_security_group" "default" {
   name   = var.name
   vpc_id = var.vpc_id
 }
+
 resource "aws_security_group_rule" "ingress" {
   type              = "ingress"
   from_port         = var.port
@@ -19,6 +20,15 @@ resource "aws_security_group_rule" "ingress" {
   security_group_id = aws_security_group.default.id
 }
 
-output "aws_security_group_id" {
+resource "aws_security_group_rule" "egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.default.id
+}
+
+output "security_group_id" {
   value = aws_security_group.default.id
 }
